6af6e528bc0d0c521ec953e02e64310022d265bd
Nicholas Karns <nkarns25@protonmail.com>
2024-09-04 15:41:59 -0400

    Serving Routes And Static Files From Directory

--- app.py
+++ app.py
@@ -1,12 +1,20 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, send_from_directory
 from flask_cors import CORS
 import json
 import subprocess
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='static')
 CORS(app)  # Enable CORS
 
 members_data = []
+
+@app.route('/')
+def home():
+    return send_from_directory(app.static_folder, 'index.html')
+
+@app.route('/<path:path>')
+def serve_static(path):
+    return send_from_directory(app.static_folder, path)
 
 @app.route('/update_members', methods=['POST'])
 def update_members():
